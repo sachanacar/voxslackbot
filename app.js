ffbe7ca084ad882aa431c5145a7854407ed01030
Sacha Nacar <sacha.slushy@gmail.com>
2016-01-09 13:04:07 +0100

    fix

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ if (req.body.token == '6P3xHipAHZkYezgbHHnQjGLj'){
 	var quantity = parameters[4];
 	searchDid(0,1,country, city, type, feature, quantity);
 } else{
-	console.log(req.body.apiModule);
+	var body = JSON.parse(req.body);
+	console.log(body.apiModule);
 	res.status(200).send('You are not authorized to reach this endpoint!');
 }
 
