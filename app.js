7f9bdf9f436f7a522435e821fd9762f947399f33
Sacha Nacar <sacha.slushy@gmail.com>
2016-01-09 12:59:16 +0100

    fix

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function searchDid(pageNumber, pageSize, countryCodeA3, cityNamePattern, didType
             console.log('[DEBUG] - DID found: '+didid);
             createCart(didid, quantity);
         } else {
-        	console.log(response.body);
+        	console.log(response.body.errors);
         	res.setHeader('Content-Type', 'application/json');
 			res.status(200).send('could not find DID matching those criteria!');
         }
